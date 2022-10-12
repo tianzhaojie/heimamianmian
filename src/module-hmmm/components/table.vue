04eb381ff08e49d8109aa24738d39b9fe2c49230
tianzhaojie <1192556828@qq.com>
2022-10-12 23:13:08 +0800

    feat：表格

---
+++ src/module-hmmm/components/table.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-table
+    v-if="tableList"
+    :data="tableList"
+    style="width: 100%"
+  >
+    <el-table-column
+      v-for="item ,index in labels"
+      :key="index"
+      :label="item[1]"
+      :width="item[2]"
+    >
+      <template slot-scope="{row}">
+        <slot :scope="row" :name="item[0]">{{ row[item[0]] }}</slot>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+
+export default {
+  name: 'Table',
+  props: {
+    tableList: {
+      type: [Object, Array],
+      default: () => { [] }
+    },
+    labels: {
+      type: Array,
+      default: () => ([])
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/module-hmmm/pages/questions.vue
+++ src/module-hmmm/pages/questions.vue
@@ -1,14 +1,51 @@
 <template>
-  <div class='container'>
-   <top-form></top-form>
+  <div class="container">
+    <top-form />
+    <el-card>
+      <Table :table-list="tableList" :labels="labels">
+        <template #question="{scope}">
+          <div v-html="scope.question" />
+        </template>
+      </Table>
+    </el-card>
   </div>
 </template>
 
 <script>
 import TopForm from '../components/TopForm.vue'
+import Table from '../components/table.vue'
+import { list } from '@/api/hmmm/questions.js'
 export default {
+  name: 'Questions',
   components: {
-    TopForm
+    TopForm,
+    Table
+  },
+  data() {
+    return {
+      tableList: [],
+      labels: [
+
+        ['number', '试题编号', '120px'],
+        ['subjectID', '学科', '141px'],
+        ['catalogID', '目录', '137px'],
+        ['questionType', '题型', '137px'],
+        ['question', '题干', '280px'],
+        ['addDate', '录入时间', '180px'],
+        ['difficulty', '难度', '137px'],
+        ['creator', '录入人', '137px']
+      ]
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    async getList() {
+      const { data } = await list()
+      console.log(data)
+      this.tableList = data.items
+    }
   }
 }
 </script>
