efb27f3d028195449ee0823f104982a2d7a9c412
dr <1561500402@qq.com>
2022-10-14 00:31:34 +0800

    feat:菜单项样式搭建完成

--- src/components/TreeTable/index.vue
+++ src/components/TreeTable/index.vue
@@ -1,36 +1,56 @@
+<!-- eslint-disable vue/require-valid-default-prop -->
 <template>
-    <el-table :data="formatData"  :row-class-name="rowClassStatus" v-loading="listLoading"  element-loading-text="给我一点时间" fit highlight-current-row
-      style="width: 100%">
-    <el-table-column v-for="(column, index) in columns" :key="column.prop" :width="column.width" :prop="column.prop"
-      :label="column.text">
+  <el-table
+    v-loading="listLoading"
+    :data="formatData"
+    :row-class-name="rowClassStatus"
+    element-loading-text="给我一点时间"
+    fit
+    highlight-current-row
+    row-key="id"
+    style="width: 100%"
+    :indent="25"
+    :default-expand-all="defaultExpandAll"
+  >
+    <el-table-column
+      v-for="(column, index) in hander"
+      :key="column.prop"
+      :width="column.width"
+      :prop="column.prop"
+      :label="column.title"
+    >
       <template slot-scope="scope">
+        <i :class="column.name[scope.row.level-1]" class="icon" />
         <expand
           v-if="column.render"
           :render="column.render"
           :row="scope.row"
           :index="index"
-          :column="column"
-        >
-        </expand>
+          :column="scope.row"
+        />
         <span v-else>
-          {{scope.row[column.value]}}
+          {{ scope.row[column.value] }}
         </span>
       </template>
     </el-table-column>
-    <el-table-column label="操作" width="260" align="center">
-      <template  slot-scope="scope">
+    <el-table-column label="操作" width="150" align="center">
+      <template slot-scope="scope">
         <el-button
-          size="mini"
           type="primary"
-          @click="handleUpdate(scope.row)">
-          修改
-        </el-button>
+          icon="el-icon-edit"
+          style="width: 36px; heigth: 36px;"
+          circle
+          class="edit"
+          @click="handleUpdate(scope.row)"
+        />
         <el-button
-          size="mini"
           type="danger"
-          @click="handleDelete(scope.row.id)">
-          删除
-        </el-button>
+          icon="el-icon-delete"
+          circle
+          style="width: 36px; heigth: 36px;"
+          class="delt"
+          @click="handleDelete(scope.row.id)"
+        />
       </template>
     </el-table-column>
   </el-table>
@@ -39,27 +59,28 @@
 <script>
 import Utils from './utils/dataTranslate.js'
 import expand from './utils/expand'
+
 export default {
-  name: 'treeTable',
+  name: 'TreeTable',
   components: { expand },
   props: {
     // 该属性是确认父组件传过来的数据是否已经是树形结构了，如果是，则不需要进行树形格式化
     treeStructure: {
       type: Boolean,
-      default: function () {
+      default: function() {
         return false
       }
     },
     data: {
       type: Array,
-      default: function () {
+      default: function() {
         return []
       }
     },
     // 这是相应的字段展示
     columns: {
       type: Array,
-      default: function () {
+      default: function() {
         return []
       }
     },
@@ -70,14 +91,20 @@ export default {
     // 是否默认展开所有树
     defaultExpandAll: {
       type: Boolean,
-      default: function () {
+      default: function() {
         return true
       }
+    },
+    // ? 渲染表头数组
+    hander: {
+      type: Array,
+      // eslint-disable-next-line vue/require-valid-default-prop
+      default: []
     }
   },
   computed: {
     // 格式化数据源
-    formatData: function () {
+    formatData: function() {
       const me = this
       if (me.treeStructure) {
         const data = Utils.treeToArray(me.data, null, null, me.defaultExpandAll)
@@ -86,16 +113,19 @@ export default {
       return me.data
     }
   },
+  created() {
+  },
   methods: {
-    rowClassStatus: function () {
+    rowClassStatus: function() {
       this.$emit('rowClassStatus')
     },
-    handleUpdate (row) {
+    handleUpdate(row) {
       this.$emit('handleUpdate', row)
     },
-    handleDelete (user) {
+    handleDelete(user) {
       this.$emit('removeUser', user)
     }
+
   }
 }
 </script>
@@ -150,5 +180,28 @@ table td {
   cursor: pointer;
   color: $color-blue;
   margin-left: -$space-width;
+}
+:deep(.el-table__expand-icon--expanded){
+  display: none !important;
+}
+:deep(.el-table__placeholder){
+  width: 0;
+}
+:deep(.cell){
+  font-size: 14px;
+  .icon{
+    font-size: 18px;
+    margin-right: 15px;
+  }
+}
+.edit{
+      color: #409eff;
+    background: #ecf5ff;
+    border-color: #b3d8ff;
+}
+.delt{
+      color: #f56c6c;
+    background: #fef0f0;
+    border-color: #fbc4c4;
 }
 </style>
--- src/module-manage/pages/menus.vue
+++ src/module-manage/pages/menus.vue
@@ -1,27 +1,125 @@
 <template>
-  <div>
-    菜单管理
+  <div class="menus">
+    <el-card>
+      <div class="rightBtn">
+        <el-button type="primary" icon="el-icon-edit" size="small" class="btn">添加菜单</el-button>
+      </div>
+      <!--
+
+      <el-row>
+        <el-table
+          ref="disabel"
+          :data="menuList"
+          style="width: 100%;dispaly:flex;"
+          row-key="pid"
+          :default-expand-all="true"
+          :tree-props="{children: 'childs', hasChildren: 'hasChildren'}"
+          :expand-row-keys="[]"
+          @expand-change="none"
+        >
+          <el-table-column
+            prop="title"
+            label="标题"
+            width="200"
+          >
+            <template slot-scope="{row}">
+              <i class="el-icon-edit" />
+              <span>{{ row.title }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            prop="code"
+            label="权限点代码"
+          />
+          <el-table-column
+            label="操作"
+            class-name="operation"
+            width="110"
+          >
+            <template>
+              <el-button type="primary" icon="el-icon-edit" circle />
+              <el-button type="danger" icon="el-icon-delete" circle />
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-row> -->
+      <TreeTable :data="menuList" :tree-structure="true" :hander="hander" />
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
 
+const arrayTreeAddLevel = (array, levelName = 'level', childrenName = 'children') => {
+  if (!Array.isArray(array)) return []
+  const recursive = (array, level = 0) => {
+    level++
+    return array.map(v => {
+      v[levelName] = level
+      const child = v[childrenName]
+      if (child && child.length) recursive(child, level)
+      return v
+    })
+  }
+  return recursive(array)
+}
+
+import TreeTable from '@/components/TreeTable'
+import { list } from '@/api/base/menus.js'
+export default {
+  components: {
+    TreeTable
+  },
+  data() {
+    return {
+      menuList: [],
+      arr: [],
+      expand: true,
+      hander: [{ title: '标题', width: 200, value: 'title', name: ['el-icon-folder-opened', 'el-icon-document', 'el-icon-view'] },
+        { title: '权限点代码', width: '', value: 'code', name: '' }],
+      indent: 20
     }
   },
-
-  created () {
-
+  async created() {
+    const { data } = await list()
+    this.menuList = JSON.parse(JSON.stringify(data).replace(/childs/g, 'children').replace(/points/g, 'children'))
+    // this.retract(this.menuList)
+    this.menuList = arrayTreeAddLevel(this.menuList)
+    console.log(arrayTreeAddLevel(this.menuList))
   },
-
   methods: {
-
+    retract(data) {
+      // 每行缩进
+      data.forEach(item => {
+        if (item.children) {
+          this.retract(item.children)
+        } else {
+          item.children = []
+        }
+      })
+      console.log(data)
+    }
   }
 }
 </script>
 
 <style scoped lang='less'>
-
+.menus{
+  padding: 20px;
+ .rightBtn{
+  display: flex;
+  justify-content: end;
+      .btn{
+    background-color: #67c23a;
+    color: #fff;
+    margin-bottom: 20px;
+  }
+ }
+.operation{
+  float: right;
+}
+/deep/.el-icon-arrow-right{
+  display: none;
+}
+}
 </style>
