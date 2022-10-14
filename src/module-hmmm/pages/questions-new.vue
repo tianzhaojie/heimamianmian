ac2671dc398a8ad10787810f268753ede603db1b
徐志豪 <1505891112@qq.com>
2022-10-14 22:14:05 +0800

    feat:页面搭建

--- src/module-hmmm/pages/questions-new.vue
+++ src/module-hmmm/pages/questions-new.vue
@@ -1,14 +1,14 @@
 <template>
   <div class='container'>
-    <el-card>
+    <el-card >
       <div slot="header" class="header">
        <span>试题录入</span>
      </div>
-      <el-form class="form" :model="formData" :rules="rules" label-width="120px">
+      <el-form class="form" :model="form" :rules="rules" label-width="120px">
         <el-form-item label="学科:">
-        <el-select v-model="value" placeholder="请选择">
-             <el-option
-               v-for="item in options"
+        <el-select v-model="form.subject" placeholder="请选择">
+           <el-option
+               v-for="item in subjects"
                :key="item.value"
                :label="item.label"
                :value="item.value">
@@ -16,42 +16,50 @@
          </el-select>
         </el-form-item>
         <el-form-item label="目录:">
-            <el-select v-model="value" placeholder="请选择">
+            <el-select v-model="form.directoryName" placeholder="请选择">
                 <el-option
-                  v-for="item in options"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value">
+                  v-for="item in directorysList"
+                  :key="item.id"
+                  :label="item.directoryName"
+                  :value="item.id">
                 </el-option>
             </el-select>
         </el-form-item>
         <el-form-item label="企业:">
-            <el-select v-model="value" placeholder="请选择">
+            <el-select v-model="form.company" placeholder="请选择">
                 <el-option
-                  v-for="item in options"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value">
+                  v-for="item in lists"
+                  :key="item.id"
+                  :label="item.company"
+                  :value="item.company">
                 </el-option>
             </el-select>
         </el-form-item>
-        <el-form-item label="城市:">
-            <el-select v-model="value" placeholder="请选择">
+        <el-form-item label="城市:" class="city">
+            <el-select v-model="form.citys" placeholder="请选择" >
                 <el-option
-                  v-for="item in options"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value">
+                  v-for="(item,index) in citys"
+                  :key="index"
+                  :label="item.city"
+                  :value="item.city">
+                </el-option>
+            </el-select>
+            <el-select v-model="form.area" placeholder="请选择" >
+                <el-option
+                  v-for="(item,index) in citys.area"
+                  :key="index"
+                  :label="item"
+                  :value="item">
                 </el-option>
             </el-select>
         </el-form-item>
         <el-form-item label="方向:">
-            <el-select v-model="value" placeholder="请选择">
+            <el-select v-model="form.direction" placeholder="请选择">
                 <el-option
-                  v-for="item in options"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value">
+                  v-for="(item,index) in direction"
+                  :key="index"
+                  :label="item"
+                  :value="item">
                 </el-option>
             </el-select>
         </el-form-item>
@@ -69,16 +77,103 @@
            <el-radio :label="9">困难</el-radio>
           </el-radio-group>
         </el-form-item>
-        <el-form-item label="题干:" style="height:239px;margin-bottom:22px">
+        <el-form-item label="题干:" class="stem" >
             <quillEditor v-model="content" :options="editorOption"  ref="myQuillEditor"
             @blur="onEditorBlur($event)" @focus="onEditorFocus($event)"
             @change="onEditorChange($event)"
             ></quillEditor>
-            <div v-html="str" class="ql-editor">
+            <!-- <div v-html="str" class="ql-editor">
             {{str}}
-            </div>
+            </div> -->
+        </el-form-item>
+        <el-form-item label="选项:">
+          <el-radio-group v-model="radio" style="width:100%">
+            <el-col class="options-item">
+            <el-radio :label="3">A:</el-radio>
+            <el-input v-model="input" style="width:240px"></el-input>
+            <el-upload
+             class="upload-demo"
+             drag
+             action="#"
+             multiple>
+             <i class="el-icon-circle-close"></i>
+            <div class="el-upload__text"><em>上传图片</em></div>
+            </el-upload>
+            </el-col>
+            <el-col class="options-item">
+            <el-radio :label="6">B:</el-radio>
+            <el-input v-model="input" style="width:240px"></el-input>
+            <el-upload
+             class="upload-demo"
+             drag
+             action="#"
+             multiple>
+             <i class="el-icon-circle-close"></i>
+            <div class="el-upload__text"><em>上传图片</em></div>
+            </el-upload>
+            </el-col>
+            <el-col class="options-item">
+            <el-radio :label="9">C:</el-radio>
+            <el-input v-model="input" style="width:240px"></el-input>
+            <el-upload
+             class="upload-demo"
+             drag
+             action="#"
+             multiple>
+             <i class="el-icon-circle-close"></i>
+            <div class="el-upload__text"><em>上传图片</em></div>
+            </el-upload>
+            </el-col>
+            <el-col class="options-item">
+            <el-radio :label="12">D:</el-radio>
+            <el-input v-model="input" style="width:240px"></el-input>
+            <el-upload
+             class="upload-demo"
+             drag
+             action="#"
+             multiple>
+             <i class="el-icon-circle-close"></i>
+            <div class="el-upload__text"><em>上传图片</em></div>
+            </el-upload>
+            </el-col>
+            </el-radio-group>
+             <el-button type="danger" disabled="disabled" size="small">+增加选项与答案</el-button>
+        </el-form-item>
+        <el-form-item label="解析视频:" class="video">
+          <el-input v-model="input" style="width:400px"></el-input>
+        </el-form-item>
+        <el-form-item label="答案解析:" class="stem" >
+            <quillEditor v-model="content" :options="editorOption"  ref="myQuillEditor"
+            @blur="onEditorBlur($event)" @focus="onEditorFocus($event)"
+            @change="onEditorChange($event)"
+            ></quillEditor>
+            <!-- <div v-html="str" class="ql-editor">
+            {{str}}
+            </div> -->
+        </el-form-item>
+        <el-form-item label="题目备注:">
+          <el-input
+           type="textarea"
+           :rows="2"
+           placeholder="请输入内容"
+           v-model="textarea">
+          </el-input>
+        </el-form-item>
+        <el-form-item label="试题标签:">
+        <el-select v-model="value" placeholder="请选择试题标签">
+           <el-option
+               v-for="item in options"
+               :key="item.value"
+               :label="item.label"
+               :value="item.value">
+             </el-option>
+         </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">确认提交</el-button>
         </el-form-item>
       </el-form>
+
     </el-card>
   </div>
 </template>
@@ -88,6 +183,11 @@ import { quillEditor } from 'vue-quill-editor' // 调用编辑器
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
+import { list } from '@/api/hmmm/companys'
+import { simple } from '@/api/hmmm/subjects'
+import { directorysList } from '@/api/hmmm/directorys'
+import { direction } from '@/api/hmmm/constants'
+import { datas } from '@/api/hmmm/citys'
 export default {
   components: {
     quillEditor
@@ -99,7 +199,7 @@ export default {
       content: '',
       str: '',
       editorOption: {
-        placeholder: '请在这里输入',
+        placeholder: '',
         modules: {
           toolbar: [
             ['bold', 'italic', 'underline', 'strike'], // 加粗，斜体，下划线，删除线
@@ -117,13 +217,51 @@ export default {
             ['image', 'link'] // 上传图片、上传视频
           ]
         }
+      },
+      page: {
+        page: 1,
+        pagesize: 10000
+      },
+      lists: [],
+      subjects: [],
+      directorysList: [],
+      direction: direction,
+      citys: datas,
+      form: {
+        subject: '',
+        directoryName: '',
+        company: '',
+        direction: '',
+        citys: '',
+        area: ''
       }
-
     }
   },
+  created () {
+    this.getcompanys()
+    this.getsubjects()
+    this.getdirectorysList()
+  },
   methods: {
+    // 获取公司信息
+    async  getcompanys () {
+      const { data: { items } } = await list(this.page)
+      console.log(items)
+      this.lists = items
+    },
+    // 获取学科
+    async getsubjects () {
+      const { data } = await simple()
+      console.log(data)
+      this.subjects = data
+    },
+    // 获取目录
+    async getdirectorysList () {
+      const { data: { items } } = await directorysList(this.page)
+      console.log(items)
+      this.directorysList = items
+    },
     onEditorReady (editor) { // 准备编辑器
-
     },
     onEditorBlur () {}, // 失去焦点事件
     onEditorFocus () {}, // 获得焦点事件
@@ -136,9 +274,6 @@ export default {
     }
   },
   computed: {
-    editor () {
-      return this.$refs.myQuillEditor.quill
-    }
   },
   mounted () {
     const content = '' // 请求后台返回的内容字符串
@@ -156,6 +291,75 @@ export default {
     :deep(.el-input__inner) {
       width: 400px;
     }
+    .city {
+
+      :deep(.el-input__inner) {
+        width: 200px;
+        margin-right: 5px;
+      }
+    }
+
+    .quill-editor {
+      height: 200px;
+      :deep(.ql-toolbar.ql-snow ){
+       height: 40px!important;
+       line-height: 40px;
+       padding: 0px 8px;
+      }
+    }
+    .stem {
+      height:239px;
+      margin-bottom:22px;
+    }
+    .options-item {
+      display: flex;
+      align-items: center;
+      height: 80px;
+      line-height: 80px;
+      padding-bottom: 20px;
+      .el-radio {
+        margin-right: 15px;
+      }
+      :deep(.el-input__inner) {
+        width: 240px!important;
+      }
+    .upload-demo {
+        margin-left: 5px;
+
+      width: 100px;
+      height: 60px;
+    :deep(.el-upload-dragger){
+      position: relative;
+      width: 100px;
+      height: 60px;
+      line-height: 60px;
+      border: 1px dashed #d9d9d9;
+      border-radius: 6px;
+      cursor: pointer;
+      .el-icon-circle-close {
+        position: absolute;
+        right: -9px;
+        top: -9px;
+        color: #999;
+        background: #fff;
+        font-size: 18px;
+        z-index: 1;
+      }
+    }
   }
-}
+    }
+  }
+.el-input--medium {
+    width: 400px;
+    height: 96px!important;
+    :deep(.el-textarea__inner) {
+      min-height: 96px!important;
+    }
+    }
+  .video {
+    :deep(.el-input__inner) {
+      width: 400px;
+    }
+  }
+  }
 </style>
